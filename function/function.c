a4facd7f6d23ab199b1196e49912f54dec9cf294
nosib55 <lmnosib555@gmail.com>
2026-07-24 01:58:30 +0600

    learning function

---
+++ function/function.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ int hello()
+ {
+    printf("hello world\n");
+
+ }
+ int main (){
+
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+    return 0;
+ }
---
+++ loops/for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int n,i;
+    printf("Enter the number of times you want to print the message: ");
+    scanf("%d", &n);
+    for (i=1; i<=n; i++)
+    {
+        printf("I LOVE YOU SAYMA \n");
+    }
+    return 0;
+}
